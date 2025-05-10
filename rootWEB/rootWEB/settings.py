1de690d83c5c78c4fa7c5d7fef2d2b4e26596903
🐧Kim Hyun Seo <152568679+khyunse0@users.noreply.github.com>
2025-05-10 23:10:22 +0900

    Update settings.py

--- rootWEB/rootWEB/settings.py
+++ rootWEB/rootWEB/settings.py
@@ -132,6 +132,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'rootWEB', 'mainApp', 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
