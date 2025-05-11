bf53d546e51beade0912bcd624e9637b93769cfd
khyunse0 <rjdls2549@donga.ac.kr>
2025-05-12 01:13:16 +0900

    add efficientNet

--- rootWEB/mainApp/apps.py
+++ rootWEB/mainApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 import torch
-import timm
+from efficientnet_pytorch import EfficientNet
 import os
 
 class MainappConfig(AppConfig):
@@ -8,16 +8,11 @@ class MainappConfig(AppConfig):
     name = "mainApp"
 
     def ready(self):
-        # 모델 파일 경로
         model_path = os.path.abspath("mainApp/static/model/alopecia_model.pt")
         print(f"MODEL_PATH >>> {model_path}")
 
-        # EfficientNet-B4 모델 정의 (timm 사용)
-        self.model = timm.create_model("efficientnet_b4", pretrained=False, num_classes=4)
-
-        # 모델 가중치 로드
-        state_dict = torch.load(model_path, map_location=torch.device('cpu'))
-        self.model.load_state_dict(state_dict)
+        # 전체 모델 로드
+        self.model = torch.load(model_path, map_location="cpu")
         self.model.eval()
 
         print("debug >>>>> EfficientNet-B4 모델 로드 완료")
